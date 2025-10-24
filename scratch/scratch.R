ad87afda8d1c15df152a1dcdfe754d40975596a6
Sam Herniman <samherniman@posteo.net>
2025-10-24 15:33:08 +0200

    started working on transit stuff

--- scratch/scratch.R
+++ scratch/scratch.R
@@ -10,3 +10,19 @@ plot(sf::st_coordinates(test_df), col = "grey")
 
 res <- dbscan::extractDBSCAN(buildings_dbscan, eps_cl = .2)
 dbscan::hullplot(sf::st_coordinates(test_df), res)
+
+# pak::pak("samherniman/autocruller")
+
+buildings_wide_df_count <-
+  buildings_wide_df |>
+  dplyr::group_by(combined_id) |>
+  dplyr::summarise(
+    n = dplyr::n(),
+    nwrname = dplyr::first(nwrname),
+    osmtag = dplyr::first(osmtag),
+    countryname = dplyr::first(countryname),
+    co2ppm = median(ppmavg)
+  ) |>
+  dplyr::slice_max(n, n = 40)
+
+mapview::mapview(buildings_wide_df_count, zcol = "n")
